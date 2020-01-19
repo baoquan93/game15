da89201a0e1e4705f6007f6f39b012acf77621fa
baoquan <baoquanb@126.com>
2020-01-19 13:40:17 +0800

    提交测试2

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+//测试使用
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
